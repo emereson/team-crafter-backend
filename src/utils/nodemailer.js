5ea0f091f9c104e8ba5b2720950dbb2d35e1ce69
Emerson Fernadez <escanorrosa123@gmail.com>
2025-12-01 16:23:19 -0500

    sss

--- src/utils/nodemailer.js
+++ src/utils/nodemailer.js
@@ -3,15 +3,15 @@ import { FRONTEND_URL } from '../../config.js';
 import logger from './logger.js';
 
 export const transporter = nodemailer.createTransport({
-  host: 'mail.team-crafter.com', // Servidor SMTP
-  port: 587, // Puerto SMTP seguro
-  secure: false, // true si usas 465, false si usas 587
+  host: 'mail.team-crafter.com',
+  port: 465, // <-- Este es el puerto correcto SMTP
+  secure: true, // <-- 465 usa conexión segura SSL/TLS
   auth: {
     user: process.env.EMAIL, // ventas@team-crafter.com
     pass: process.env.PASSWORD_EMAIL, // contraseña del correo
   },
   tls: {
-    rejectUnauthorized: false, // evita errores si el certificado SSL no es válido
+    rejectUnauthorized: false,
   },
 });
 
